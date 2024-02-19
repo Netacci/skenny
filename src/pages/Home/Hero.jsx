d83b81877c0be83564becc6eeb27fab23c31dd56
Chineta Adinnu <chinetaadinnu@gmail.com>
2024-02-19 11:36:40 +0100

    update hero text

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -5,24 +5,36 @@ const Hero = () => {
     <div className=' px-4 lg:px-14 py-12 flex gap-8 bg-[#fbe9e7] flex-col lg:flex-row'>
       <div className='lg:w-[80%] w-full'>
         <Typography
-          variant='h2'
+          variant='h3'
           color='blue-gray'
-          className='mb-2 text-2xl lg:text-5xl'
+          className='mb-2 text-xl lg:text-3xl'
         >
-          Your dream home awaits you!
+          LOOKING FOR PROPERTY INVESTMENT OPPORTUINITIES?
         </Typography>
         <Typography
           color='gray'
           className='font-medium text-[14px] lg:text-[16px] leading-7 mt-3'
         >
-          At SKENNY HEIGHTS we are a new property Deal Sourceror, management and
-          consultants in Property Investment Portfolios in the United Kingdom,
-          with greater tracks in Africa (Nigeria); our team have a track record
-          of turning property management challenges into a seamless solutions.
-          Our goals are in tandem with the desires of all property investors;
-          Maximizing Return On Investment (ROI) and we accomplish this task by
-          applying exceptional property-management skills, all while ensuring
-          Tenants satisfaction through top-notch property maintenance.
+          At Skenny Properties our managerial skills are tailored to help
+          investors or just one-off buyer navigate through the often difficult
+          metrics of property business such as location, property pricing,
+          rental yields, tenant demands, career opportunities, population and
+          Buy-to-Let opportuinities.
+        </Typography>
+        <Typography
+          color='gray'
+          className='font-medium text-[14px] lg:text-[16px] leading-7 mt-3'
+        >
+          We make property investment a rewarding endeavor for property
+          investors, be it a small scale one off or upscale corporations.
+        </Typography>
+        <Typography
+          color='gray'
+          className='font-medium text-[14px] lg:text-[16px] leading-7 mt-3'
+        >
+          Why not contact us today to begin that asset acquisition and wealth
+          diversification quest and ultimately kickstarting your long term plan
+          for early retirement?
         </Typography>
       </div>
       <Carousel
